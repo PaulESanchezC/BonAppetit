ee57d639eae15c4849197f6ff1f44e84a05676ee
TheRealPaulyglot <PaulESanchezC@outlook.com>
2022-03-31 14:59:32 -0400

    Table Controller Tests Begin

---
+++ MicroServices/BonAppetit.RestaurantServices/ApiControllersTest/TableControllerTest.cs
@@ -0,0 +1,21 @@
+﻿using Moq;
+using NUnit.Framework;
+using RestaurantApi.Controllers;
+using Services.Repository.TableRepository;
+
+namespace ApiControllersTest;
+
+[TestFixture]
+public class TableControllerTest
+{
+    private TableController _tableController;
+    private Mock<ITableService> _tableService;
+
+    [SetUp]
+    public void Setup()
+    {
+        _tableService = new();
+        _tableController = new(_tableService.Object);
+    }
+
+}
