d9c037bbde55638bb27a103fd8c26ed7638e43e7
TheRealPaulyglot <PaulESanchezC@outlook.com>
2022-04-06 23:04:51 -0400

    Removed Legacy

--- BonAppetitRestaurantManagerApp/StaticData/LocalStorage.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace StaticData;
-
-public static class LocalStorage
-{
-    public const string JwtToken = "JwtToken";
-    public const string AppId = "Bon Appetit Restaurant Manager App";
-}
