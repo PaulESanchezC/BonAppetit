aba41e327a2f5236a76ad0de273c1be81bcd45c5
TheRealPaulyglot <PaulESanchezC@outlook.com>
2022-04-18 03:17:35 -0400

    database authentication server

---
+++ MicroServices/BonAppetit.AuthenticationService/Data/Migrations/20220417231452_Create_RestaurantManagers.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Data.Migrations
+{
+    public partial class Create_RestaurantManagers : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Managers",
+                columns: table => new
+                {
+                    RestaurantId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    ApplicationUserId = table.Column<string>(type: "nvarchar(450)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Managers", x => x.RestaurantId);
+                    table.ForeignKey(
+                        name: "FK_Managers_AspNetUsers_ApplicationUserId",
+                        column: x => x.ApplicationUserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Managers_ApplicationUserId",
+                table: "Managers",
+                column: "ApplicationUserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Managers");
+        }
+    }
+}
